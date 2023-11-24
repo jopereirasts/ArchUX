3ec4df1a2b8d73319339dcc1ec0ae78249824f29
[jo] <jonespjunior@gmail.com>
2023-11-24 16:22:13 -0400

    criei rotas, pre criacao da navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Home from './components/Home/Home';
 
-function App() {
-  
-
-  return (
-    <>
-      <div className='banner'>
-        ArchUX
-      </div>
-      <div className='divisor'></div>
-      <h1>Bem-vindo ao ArchUX</h1>
-      <div className='paragraph'>
-        O Nosso Objetivo é reunir em um 
-          só lugar informações sobre 
-          Experiência do Usuário (UX) e 
-          Arquitetura da Informação, com o 
-          foco nos Elementos da UX de Garret. 
-          Além de apresentar possíveis técnicas 
-          de como aplicar cada um dos níveis e 
-          também uma técnica de inspeção de 
-          sistemas baseada no estudo do Garrett.
-      </div>
-      <div className='divisor'></div>
-      <h2> User Experience (UX) </h2>
-      <h2> Arquitetura da Informação (UX) </h2>
-      <h2> Planos de Garrett (UX) </h2>
-      <h2> Avaliação (UX) </h2>
-      <h2> Design (UX) </h2>
-      
-    </>
-  )
+export default function App() {
+ return (
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
+  </BrowserRouter>
+ ) 
 }
-
-export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,58 @@
+import '../../App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='banner'>
+        ArchUX
+      </div>
+      <div className='divisor'></div>
+      <h1>Bem-vindo ao ArchUX</h1>
+      <p>
+        O Nosso Objetivo é reunir em um 
+          só lugar informações sobre 
+          Experiência do Usuário (UX) e 
+          Arquitetura da Informação, com o 
+          foco nos Elementos da UX de Garret. 
+          Além de apresentar possíveis técnicas 
+          de como aplicar cada um dos níveis e 
+          também uma técnica de inspeção de 
+          sistemas baseada no estudo do Garrett.
+      </p>
+      <div className='divisor'></div>
+      <h2> User Experience (UX) </h2>
+      <p>
+      User Experience (UX) refere-se à experiência geral de um usuário ao interagir com um produto ou serviço digital. Envolve a usabilidade, acessibilidade, eficiência e satisfação do usuário. A UX busca criar interfaces intuitivas, agradáveis e eficazes, considerando a jornada do usuário para proporcionar experiências positivas e significativas. Ver mais. 
+      </p>
+      <div className='divisor'></div>
+      <h2> Arquitetura da Informação (UX) </h2>
+      <p>
+      É uma disciplina que envolve o planejamento e organização de informações de forma lógica e acessível em sistemas digitais, sites e aplicativos. Ela visa melhorar a usabilidade, tornando mais fácil para os usuários encontrar e compreender o conteúdo, promovendo experiências mais eficazes e satisfatórias. Ver mais. 
+      </p>
+      <div className='divisor'></div>
+      <h2> Planos de Garrett (UX) </h2>
+      <p>
+      Os Planos de Garrett, desenvolvidos por Jesse James Garrett, são uma estrutura amplamente utilizada no design de experiência do usuário (UX). Eles descrevem uma série de diagramas que ajudam a visualizar e comunicar as fases de um projeto de design, desde a compreensão inicial do problema até a implementação da solução, incluindo pesquisa, design, desenvolvimento e teste. Esses planos auxiliam na organização e na garantia de que a visão do projeto seja bem-sucedida. Ver mais.
+      </p>
+      <div className='divisor'></div>
+      <h2> Avaliação (UX) </h2>
+      <p>
+        15 heurísticas  foram criadas e organizadas de acordo cada um do Planos de Garrett para avaliação de sistemas.
+        Ver mais.
+      </p>
+      <div className='divisor'></div>
+      <h2> Design (UX) </h2>
+      <p>
+      Sugestão de Técnicas para utilizar no design de aplicações, de acordo com cada um dos planos Garrett.
+      Veremos mais em breve. :)
+      </p>
+      {/*TODO
+      FAZER ACCORDION DA PAGINA PRINCIPAL*/}
+      
+    </>
+  )
+}
+
+export default App
