7061e4b90153bf0720e49ee101753d0812102996
[jo] <jonespjunior@gmail.com>
2023-11-25 11:02:33 -0400

    mudanças na navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,45 +10,47 @@ export default function Home() {
       <div className='divisor'></div>
       <h1>Bem-vindo ao ArchUX</h1>
       <p className='text'>
-        O Nosso Objetivo é reunir em um 
-          só lugar informações sobre 
-          Experiência do Usuário (UX) e 
-          Arquitetura da Informação, com o 
-          foco nos Elementos da UX de Garret. 
-          Além de apresentar possíveis técnicas 
-          de como aplicar cada um dos níveis e 
-          também uma técnica de inspeção de 
-          sistemas baseada no estudo do Garrett.
+        O Nosso Objetivo é reunir em um só lugar informações sobre Experiência do Usuário (UX) e 
+          Arquitetura da Informação, com o foco nos Elementos da UX de Garret. Além de apresentar possíveis técnicas 
+          de como aplicar cada um dos níveis e também uma técnica de inspeção de sistemas baseada no estudo do Garrett.
       </p>
       <div className='divisor'></div>
       <h2> User Experience (UX) </h2>
       <p className='text'>
-      User Experience (UX) refere-se à experiência geral de um usuário ao interagir com um produto ou serviço digital. Envolve a usabilidade, acessibilidade, eficiência e satisfação do usuário. A UX busca criar interfaces intuitivas, agradáveis e eficazes, considerando a jornada do usuário para proporcionar experiências positivas e significativas. 
-      <Link to={'/ux'}> Ver Mais</Link> 
+      User Experience (UX) refere-se à experiência geral de um usuário ao interagir com um produto ou serviço 
+      digital. Envolve a usabilidade, acessibilidade, eficiência e satisfação do usuário. A UX busca criar 
+      interfaces intuitivas, agradáveis e eficazes, considerando a jornada do usuário para proporcionar experiências 
+      positivas e significativas. <Link to={'/ux'}> Ver Mais</Link> 
       </p>
       <div className='divisor'></div>
       <h2> Arquitetura da Informação (AI) </h2>
       <p className='text'>
-      É uma disciplina que envolve o planejamento e organização de informações de forma lógica e acessível em sistemas digitais, sites e aplicativos. Ela visa melhorar a usabilidade, tornando mais fácil para os usuários encontrar e compreender o conteúdo, promovendo experiências mais eficazes e satisfatórias. 
-      <Link to={'/ai'}> Ver Mais</Link> 
+      É uma disciplina que envolve o planejamento e organização de informações de forma lógica e acessível em 
+      sistemas digitais, sites e aplicativos. Ela visa melhorar a usabilidade, tornando mais fácil para os 
+      usuários encontrar e compreender o conteúdo, promovendo experiências mais 
+      eficazes e satisfatórias. <Link to={'/ai'}> Ver Mais</Link> 
       </p>
       <div className='divisor'></div>
       <h2> Planos de Garrett </h2>
       <p className='text'>
-      Os Planos de Garrett, desenvolvidos por Jesse James Garrett, são uma estrutura amplamente utilizada no design de experiência do usuário (UX). Eles descrevem uma série de diagramas que ajudam a visualizar e comunicar as fases de um projeto de design, desde a compreensão inicial do problema até a implementação da solução, incluindo pesquisa, design, desenvolvimento e teste. Esses planos auxiliam na organização e na garantia de que a visão do projeto seja bem-sucedida. 
-      <Link to={'/garrett'}> Ver Mais</Link> 
+      Os Planos de Garrett, desenvolvidos por Jesse James Garrett, são uma estrutura amplamente utilizada 
+      no design de experiência do usuário (UX). Eles descrevem uma série de diagramas que ajudam a visualizar 
+      e comunicar as fases de um projeto de design, desde a compreensão inicial do problema até a implementação 
+      da solução, incluindo pesquisa, design, desenvolvimento e teste. Esses planos auxiliam na organização e na 
+      garantia de que a visão do projeto 
+      seja bem-sucedida. <Link to={'/garrett'}> Ver Mais</Link> 
       </p>
       <div className='divisor'></div>
       <h2> Avaliação</h2>
       <p className='text'>
-        15 heurísticas  foram criadas e organizadas de acordo cada um do Planos de Garrett para avaliação de sistemas.
-        <Link to="/heuristicas"> Ver Mais.</Link>
+        15 heurísticas  foram criadas e organizadas de acordo cada um do Planos de Garrett para avaliação 
+        de sistemas. <Link to="/heuristicas"> Ver Mais.</Link>
       </p>
       <div className='divisor'></div>
       <h2> Design </h2>
       <p className='text'>
-      Sugestão de Técnicas para utilizar no design de aplicações, de acordo com cada um dos planos Garrett.
-      <Link to="/tecnicas"> Ver mais.</Link>
+      Sugestão de Técnicas para utilizar no design de aplicações, de acordo com cada um 
+      dos planos Garrett. <Link to="/tecnicas"> Ver mais.</Link>
       </p>      
     </>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,17 @@ function Navbar() {
   return (
     <nav>
       <Link to="/">Home</Link>
+      <div className='barDivisor'></div>
       <Link to="/ux">User Experience</Link>
+      <div className='barDivisor'></div>
       <Link to="/ai">Arquitetura da Informação</Link>
+      <div className='barDivisor'></div>
       <Link to="/garrett">Planos de Garrett</Link>
+      <div className='barDivisor'></div>
       <Link to="/heuristicas">Heurísticas</Link>
+      <div className='barDivisor'></div>
       <Link to="/tecnicas">Técnicas</Link>
+      <div className='barDivisor'></div>
       <Link to="/about">Sobre Nós</Link>
     </nav>
   );
